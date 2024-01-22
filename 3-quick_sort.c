288b22a23a1f36d36618ce5a3b20314ba5fba637
kilokomuli <kilokoemma175@gmail.com>
2024-01-22 00:58:07 -0800

    errorfix--

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -30,22 +30,28 @@ void swap_ints(int *a, int *b)
 
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
-	int pivot = array[high];
-	int i = low - 1;
-	int j;
+	int *pivot, above, below;
 
-	for (j = low; j <= high - 1; ++j)
+	pivot = array + high;
+	for (above = below = low; below < high - 1; below++)
 	{
-		if (array[j] <= pivot)
+		if (array[below] < *pivot)
 		{
-			++i;
-			swap_ints(&array[i], &array[j]);
-			print_array(array, size);
+			if (above < below)
+			{
+				swap_ints(array + below, array + above);
+				print_array(array, size);
+			}
+			above++;
 		}
 	}
-	swap_ints(&array[i + 1], &array[high]);
-	print_array(array, size);
-	return (i + 1);
+	if (array[above] > *pivot)
+	{
+		swap_ints(array + above, pivot);
+		
+		print_array(array, size);
+	}
+	return (above);
 }
 
 /**
@@ -58,9 +64,11 @@ int lomuto_partition(int *array, int low, int high, size_t size)
 
 void quick_sort_recursive(int *array, int low, int high, size_t size)
 {
-	if (low < high)
+	int partition;
+
+	if (low - high > 0)
 	{
-		int partition = lomuto_partition(array, low, high, size);
+		partition = lomuto_partition(array, low, high, size);
 
 		quick_sort_recursive(array, low, partition - 1, size);
 		quick_sort_recursive(array, partition + 1, high, size);
