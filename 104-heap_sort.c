2d9e4d7620cba018e9138e93839ce5422804e44a
kilokomuli <kilokoemma175@gmail.com>
2024-01-23 01:37:03 -0800

    correction

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -19,23 +19,24 @@ void swap_ints(int *a, int *b)
  * heapify - Turn a birnary to acomplete binary heap
  * @array: array to be heapified
  * @size: Number of elements in the array
+ * @base: index of the base row of the tree
  * @root: Index of the root of the heap
  */
-void heapify(int *array, size_t size, size_t root)
+void heapify(int *array, size_t size, size_t base, size_t root)
 {
 	size_t largest = root;
 	size_t left = 2 * root + 1;
 	size_t right = 2 * root + 2;
 
-	if (left < size && array[left] > array[largest])
+	if (left < base && array[left] > array[largest])
 		largest = left;
-	if (right < size && array[right] > array[largest])
+	if (right < base && array[right] > array[largest])
 		largest = right;
 	if (largest != root)
 	{
 		swap_ints(array + root, array + largest);
 		print_array(array, size);
-		heapify(array, size, largest);
+		heapify(array, size, base, largest);
 	}
 }
 
@@ -52,13 +53,13 @@ void heap_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 	for (i = (size / 2) - 1; i >= 0; i--)
-		heapify(array, size, i);
+		heapify(array, size, size, i);
 	for (i = size - 1; i > 0; i--)
 	{
 		/** Move current root to the end */
 		swap_ints(array, array + i);
 		print_array(array, size);
 		/* Call max heapify on the reduced heap */
-		heapify(array, i, 0);
+		heapify(array, size, i, 0);
 	}
 }
