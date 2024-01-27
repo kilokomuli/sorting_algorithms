cb4361a9f3cd16d03cf5198d061970fd12af6788
emmmamuli <kilokoemma175@gmail.com>
2024-01-27 08:42:39 -0800

    task10

---
+++ 103-merge_sort.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+
+/**
+ * merge_sort - sorts an arry of integers in ascending order using
+ * the merge sort algorithm
+ * array: Array to sort
+ * @size: Size of the array
+ */
+void merge_sort(int *array, size_t size);
+
